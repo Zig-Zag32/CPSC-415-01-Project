ba0677b9f8946fb9a77afe92e2558663b26320b7
George Zack <gzack31415@gmail.com>
2024-03-26 15:44:14 -0400

    Add subfolder with SpringInitializr files for the Kitchen Inventory Microservice

---
+++ Kitchen-Inventory-Microservice/src/main/java/com/example/cloudnativefinalproject/CloudNativeFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudnativefinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNativeFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudNativeFinalProjectApplication.class, args);
+    }
+
+}
